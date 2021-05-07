0e78d7a66b71327dc767336141d95b71a5ada045
kh-b1 <zmfozjko0465@gmail.com>
2021-05-07 11:45:39 +0900

    충돌이 왜 날까?

--- HealthEat/src/com/kh/tm1/run/Run.java
+++ HealthEat/src/com/kh/tm1/run/Run.java
@@ -8,6 +8,9 @@ public class Run {
 		System.out.println("오늘은 5월 7일 금요일입니다. 날씨는 비가 조금 내립니다.");
 		System.out.println("오늘은 Collection 배우는 날");
 		System.out.println("자 이제 시작이야 시간이 됐어");
+		
+		// 왜 자꾸 충돌이 난다고 할까?
+		
 	}
 
 }
