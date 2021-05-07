e9843c17666809e478c4be4e3fe3c6e1535f3ad7
kh-b1 <zmfozjko0465@gmail.com>
2021-05-07 11:53:53 +0900

    충돌중

--- HealthEat/src/com/kh/tm1/run/Run.java
+++ HealthEat/src/com/kh/tm1/run/Run.java
@@ -3,13 +3,18 @@ package com.kh.tm1.run;
 public class Run {
 
 	public static void main(String[] args) {
-		//git hub test 
+		//git hub test
+		//git checkout master
+		//git pull
+		//git checkout your-branch
+		//git merge master
 		// 안녕하세요. 방태현 입니다. 오늘은 비가 폭우처럼 내립니다. 집에 가기 힘들거 같아요.
 		System.out.println("오늘은 5월 7일 금요일입니다. 날씨는 비가 조금 내립니다.");
 		System.out.println("오늘은 Collection 배우는 날");
 		System.out.println("자 이제 시작이야 시간이 됐어");
 		
 		// 왜 자꾸 충돌이 난다고 할까?
+		
 		
 	}
 
