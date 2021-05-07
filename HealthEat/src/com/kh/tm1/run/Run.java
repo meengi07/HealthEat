edc1ff9b9b06cf6eb3c63ad3c75d5187242171ee
kh-b1 <zmfozjko0465@gmail.com>
2021-05-07 11:55:11 +0900

    git 주석 지우기

--- HealthEat/src/com/kh/tm1/run/Run.java
+++ HealthEat/src/com/kh/tm1/run/Run.java
@@ -3,11 +3,6 @@ package com.kh.tm1.run;
 public class Run {
 
 	public static void main(String[] args) {
-		//git hub test
-		//git checkout master
-		//git pull
-		//git checkout your-branch
-		//git merge master
 		// 안녕하세요. 방태현 입니다. 오늘은 비가 폭우처럼 내립니다. 집에 가기 힘들거 같아요.
 		System.out.println("오늘은 5월 7일 금요일입니다. 날씨는 비가 조금 내립니다.");
 		System.out.println("오늘은 Collection 배우는 날");
