d414a2213d5d2cbf6b204846b576a82790f03818
kh-b1 <zmfozjko0465@gmail.com>
2021-05-07 10:49:40 +0900

    print 구문 추가 해봄

--- HealthEat/src/com/kh/tm1/run/Run.java
+++ HealthEat/src/com/kh/tm1/run/Run.java
@@ -4,7 +4,8 @@ public class Run {
 
 	public static void main(String[] args) {
 		//git hub test 
-		// 안녕하세요. 방태현 입니다.
+		// 안녕하세요. 방태현 입니다. 오늘은 비가 폭우처럼 내립니다. 집에 가기 힘들거 같아요.
+		System.out.println("오늘은 5월 7일 금요일입니다. 날씨는 비가 조금 내립니다.");
 	}
 
 }
