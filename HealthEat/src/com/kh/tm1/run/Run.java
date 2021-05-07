64c9377cf2d87749d9a300247e35f8295a3c6fc0
kh-b1 <zmfozjko0465@gmail.com>
2021-05-07 11:00:31 +0900

    수업 시작

--- HealthEat/src/com/kh/tm1/run/Run.java
+++ HealthEat/src/com/kh/tm1/run/Run.java
@@ -7,6 +7,7 @@ public class Run {
 		// 안녕하세요. 방태현 입니다. 오늘은 비가 폭우처럼 내립니다. 집에 가기 힘들거 같아요.
 		System.out.println("오늘은 5월 7일 금요일입니다. 날씨는 비가 조금 내립니다.");
 		System.out.println("오늘은 Collection 배우는 날");
+		System.out.println("자 이제 시작이야 시간이 됐어");
 	}
 
 }
