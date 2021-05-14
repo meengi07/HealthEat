83e0515384dd6641ebfd9dcb496e87487e6e64f4
soyoungcareer <soyoungcareer@gmail.com>
2021-05-14 10:23:13 +0900

    soyoung test1

--- HealthEat/src/com/kh/mini/model/vo/Book.java
+++ HealthEat/src/com/kh/mini/model/vo/Book.java
@@ -2,4 +2,51 @@ package com.kh.mini.model.vo;
 
 public class Book {
 
+	private String title;
+	private String author;
+	private String publisher;
+	
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book(String title, String author, String publisher) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Title : " + title + ", Author : " + author + ", Publisher : " + publisher;
+	}
+	
+	
+	
 }
