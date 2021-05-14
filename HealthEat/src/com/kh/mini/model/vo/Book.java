7fd3e459b5d08c3853ae40eebdc9e5925ff1d1aa
김경준 <@58.123.209.11>
2021-05-14 10:22:18 +0900

    변경

--- HealthEat/src/com/kh/mini/model/vo/Book.java
+++ HealthEat/src/com/kh/mini/model/vo/Book.java
@@ -2,4 +2,50 @@ package com.kh.mini.model.vo;
 
 public class Book {
 
+	private String kkjname;
+	private int kkjprice;
+	private int kkjnum;
+
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getKkjname() {
+		return kkjname;
+	}
+
+	public void setKkjname(String kkjname) {
+		this.kkjname = kkjname;
+	}
+
+	public int getKkjprice() {
+		return kkjprice;
+	}
+
+	public void setKkjprice(int kkjprice) {
+		this.kkjprice = kkjprice;
+	}
+
+	public int getKkjnum() {
+		return kkjnum;
+	}
+
+	public void setKkjnum(int kkjnum) {
+		this.kkjnum = kkjnum;
+	}
+
+	public Book(String kkjname, int kkjprice, int kkjnum) {
+		super();
+		this.kkjname = kkjname;
+		this.kkjprice = kkjprice;
+		this.kkjnum = kkjnum;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return kkjname + " " + kkjprice + " " + kkjnum;
+	}
+
+	
 }
