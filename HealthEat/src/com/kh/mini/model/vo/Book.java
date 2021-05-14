3c3590a5fa1d41797db391e4320fa770166b2e05
mingi <meengi07@gmail.com>
2021-05-14 10:39:51 +0900

    테슷트

--- HealthEat/src/com/kh/mini/model/vo/Book.java
+++ HealthEat/src/com/kh/mini/model/vo/Book.java
@@ -2,43 +2,6 @@ package com.kh.mini.model.vo;
 
 public class Book {
 
-	private String name;
-	private String bookNum;
-	
-	public Book() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public void mingi() {
-		//김민기 
-	}
-
-	public Book(String name, String bookNum) {
-		super();
-		this.name = name;
-		this.bookNum = bookNum;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getBookNum() {
-		return bookNum;
-	}
-
-	public void setBookNum(String bookNum) {
-		this.bookNum = bookNum;
-	}
-
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return "name : " + name + "bookNum : " + bookNum; 
-	}
+	//테슷트해보겠읍니
 	
 }
