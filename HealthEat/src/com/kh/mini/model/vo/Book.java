3890beeaf4a91cc08d4ad066f73f97603f427316
mingi <meengi07@gmail.com>
2021-05-14 10:25:43 +0900

    커밋업

--- HealthEat/src/com/kh/mini/model/vo/Book.java
+++ HealthEat/src/com/kh/mini/model/vo/Book.java
@@ -12,4 +12,33 @@ public class Book {
 	public void mingi() {
 		//김민기 
 	}
+
+	public Book(String name, String bookNum) {
+		super();
+		this.name = name;
+		this.bookNum = bookNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBookNum() {
+		return bookNum;
+	}
+
+	public void setBookNum(String bookNum) {
+		this.bookNum = bookNum;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "name : " + name + "bookNum : " + bookNum; 
+	}
+	
 }
