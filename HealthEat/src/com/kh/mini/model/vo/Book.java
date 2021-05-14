7af05d490810ad80bd5672dfc26a2d1893aa8add
mingi <meengi07@gmail.com>
2021-05-14 10:19:13 +0900

    민기책 테스

--- HealthEat/src/com/kh/mini/model/vo/Book.java
+++ HealthEat/src/com/kh/mini/model/vo/Book.java
@@ -2,4 +2,14 @@ package com.kh.mini.model.vo;
 
 public class Book {
 
+	private String name;
+	private String bookNum;
+	
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void mingi() {
+		//김민기 
+	}
 }
