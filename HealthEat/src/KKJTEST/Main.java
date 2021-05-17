10afd7ae9969ff3042b9106b6fa06684c5e9af76
Notebook <Notebook@DESKTOP-DVU2BRD>
2021-05-17 12:40:03 +0900

    ㅂㄹㄴㅇㅂㅇ

---
+++ HealthEat/src/KKJTEST/Main.java
@@ -0,0 +1,294 @@
+package KKJTEST;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Main extends JFrame {
+
+	int count = 0;
+	String counts = "123";
+
+	public Main() {
+
+		this.setTitle("마이페이지 UI TEST");
+		this.setSize(500, 700);
+		// BorderLayout layout = new BorderLayout();
+		this.setLayout(null);
+
+		JPanel panel1 = new JPanel(); // 메인 페이지
+		JPanel panel2 = new JPanel(); // 건강 체크 페이지
+		JPanel panel3 = new JPanel(); // 게시판 페이지
+
+		JPanel panel4 = new JPanel(); // 건강 관리 페이지
+		JPanel panel5 = new JPanel(); // 건강 관리 페이지
+		JPanel panel6 = new JPanel(); // 건강 관리 페이지
+
+		panel1.setVisible(true);
+		panel2.setVisible(false);
+		panel3.setVisible(false);
+		panel4.setVisible(false);
+		panel5.setVisible(false);
+
+		// 메인 페이지 설정
+
+		JButton btn1 = new JButton("건강 체크");
+		JButton btn2 = new JButton("리뷰 게시판");
+		panel1.setLayout(null);
+		panel1.setBounds(0, 0, 500, 700);
+		btn1.setBounds(70, 200, 150, 100);
+		btn1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel1.setVisible(false);
+				panel2.setVisible(true);
+				panel3.setVisible(false);
+
+			}
+
+		});
+		panel1.add(btn1);
+		btn2.setBounds(230, 200, 150, 100);
+		btn2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel1.setVisible(false);
+				panel3.setVisible(true);
+			}
+
+		});
+		panel1.add(btn2);
+
+		// 건강 관리 페이지 설정
+
+		panel2.setLayout(null);
+		panel2.setBounds(0, 0, 500, 700);
+		JLabel label1 = new JLabel("건강 체크를 시작하시곘습니까?");
+		JButton btn3 = new JButton("YES");
+
+		JButton btn4 = new JButton("No");
+		btn3.setBounds(120, 300, 100, 100);
+		btn3.addActionListener(new ActionListener() {
+
+			// 미구현
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				panel2.setVisible(false);
+				panel4.setVisible(true);
+			}
+
+		});
+		btn4.setBounds(270, 300, 100, 100);
+		btn4.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel1.setVisible(true);
+				panel2.setVisible(false);
+				panel3.setVisible(false);
+			}
+
+		});
+		label1.setBounds(150, 200, 300, 100);
+		panel2.add(label1);
+		panel2.add(btn3);
+		panel2.add(btn4);
+
+		// 건강관리 페이지1 _ 문항 START
+
+		panel4.setLayout(null);
+		panel4.setBounds(0, 0, 500, 700);
+		JLabel label10 = new JLabel("주 음주 횟수");
+		JCheckBox box1 = new JCheckBox("주 1회");
+		box1.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count += 1;
+				System.out.println(count);
+
+			}
+
+		});
+
+		JCheckBox box2 = new JCheckBox("주 2~3회");
+		box2.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count += 2;
+				System.out.println(count);
+
+			}
+
+		});
+		JCheckBox box3 = new JCheckBox("주 4~5회");
+		box3.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count += 3;
+				System.out.println(count);
+
+			}
+
+		});
+		JCheckBox box4 = new JCheckBox("매일");
+		box4.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count += 4;
+				System.out.println(count);
+
+			}
+
+		});
+
+		label10.setBounds(20, 5, 100, 100);
+		box1.setBounds(60, 60, 100, 50);
+		box2.setBounds(60, 100, 100, 50);
+		box3.setBounds(60, 140, 100, 50);
+		box4.setBounds(60, 180, 100, 50);
+
+		JLabel label11 = new JLabel("하루 담배 횟수");
+		JCheckBox box5 = new JCheckBox("1~5개피");
+		box5.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count += 1;
+				System.out.println(count);
+			}
+
+		});
+		JCheckBox box6 = new JCheckBox("5~10개피");
+		box6.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count += 2;
+				System.out.println(count);
+
+			}
+
+		});
+		JCheckBox box7 = new JCheckBox("10~15개피");
+		box7.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count += 3;
+				System.out.println(count);
+
+			}
+
+		});
+		JCheckBox box8 = new JCheckBox("한 갑");
+		box8.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count += 4;
+				System.out.println(count);
+
+			}
+
+		});
+
+		label11.setBounds(250, 5, 100, 100);
+		box5.setBounds(290, 60, 100, 50);
+		box6.setBounds(290, 100, 100, 50);
+		box7.setBounds(290, 140, 100, 50);
+		box8.setBounds(290, 180, 100, 50);
+
+		JButton btn10 = new JButton("취소");
+		JButton btn11 = new JButton("다음");
+
+		btn10.setBounds(140, 610, 80, 50);
+		btn11.setBounds(240, 610, 80, 50);
+
+		btn10.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel4.setVisible(false);
+				panel1.setVisible(true);
+
+			}
+
+		});
+
+		btn11.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel4.setVisible(false);
+				panel5.setVisible(true);
+				counts = Integer.toString(count);
+				
+				// 점수 체크 메뉴
+				JButton btn20 = new JButton("확인");
+				btn20.setLocation(200, 400);
+				btn20.setSize(100,50);
+				btn20.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						panel5.setVisible(false);
+						panel1.setVisible(true);
+					}
+					
+				});
+				panel5.setLayout(null);
+				panel5.setBounds(0, 0, 500, 700);
+				JLabel label15 = new JLabel("건강 점수 : " +  counts);
+				label15.setLocation(200, 100);
+				label15.setSize(100, 100);
+				panel5.add(label15);
+				panel5.add(btn20);
+
+			}
+
+		});
+
+		panel4.add(label10);
+		panel4.add(label11);
+		panel4.add(box1);
+		panel4.add(box2);
+		panel4.add(box3);
+		panel4.add(box4);
+		panel4.add(box5);
+		panel4.add(box6);
+		panel4.add(box7);
+		panel4.add(box8);
+		panel4.add(btn10);
+		panel4.add(btn11);
+
+		// 컨테이너에 메인 페이지 panel 추가
+		this.add(panel1);
+		this.add(panel2);
+		this.add(panel3);
+		this.add(panel4);
+		this.add(panel5);
+
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+
+		new Main();
+	}
+
+}
