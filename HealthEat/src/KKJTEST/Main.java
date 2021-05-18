6997b45b5d15d5dc2df772f00942d7ec7e61c7cf
Notebook <Notebook@DESKTOP-DVU2BRD>
2021-05-18 13:01:49 +0900

    김경준

--- HealthEat/src/KKJTEST/Main.java
+++ HealthEat/src/KKJTEST/Main.java
@@ -27,15 +27,41 @@ public class Main extends JFrame {
 		JPanel panel2 = new JPanel(); // 건강 체크 페이지
 		JPanel panel3 = new JPanel(); // 게시판 페이지
 
-		JPanel panel4 = new JPanel(); // 건강 관리 페이지
-		JPanel panel5 = new JPanel(); // 건강 관리 페이지
-		JPanel panel6 = new JPanel(); // 건강 관리 페이지
+		JPanel panel4 = new JPanel(); // 건강 체크 페이지
+		JPanel panel5 = new JPanel(); // 건강 확인 페이지
+		JPanel panel6 = new JPanel(); // 건강 체크 페이지2
 
 		panel1.setVisible(true);
 		panel2.setVisible(false);
 		panel3.setVisible(false);
 		panel4.setVisible(false);
 		panel5.setVisible(false);
+		panel6.setVisible(false);
+
+		//
+
+		// 점수체크
+		// 점수 체크 메뉴
+		JButton btn20 = new JButton("확인");
+		btn20.setLocation(200, 400);
+		btn20.setSize(100, 50);
+		btn20.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel5.setVisible(false);
+				panel1.setVisible(true);
+
+			}
+
+		});
+		panel5.setLayout(null);
+		panel5.setBounds(0, 0, 500, 700);
+		JLabel label15 = new JLabel("건강 점수 및 코맨트 : " + counts);
+		label15.setLocation(100, 100);
+		label15.setSize(500, 100);
+		panel5.add(label15);
+		panel5.add(btn20);
 
 		// 메인 페이지 설정
 
@@ -227,19 +253,18 @@ public class Main extends JFrame {
 
 			@Override
 			public void itemStateChanged(ItemEvent e) {
-				count += 0;
+				count += 1;
 			}
 
 		});
-		
+
 		label12.setBounds(20, 280, 200, 100);
 		box9.setBounds(60, 340, 100, 50);
 		box10.setBounds(60, 380, 100, 50);
 		panel4.add(label12);
 		panel4.add(box9);
 		panel4.add(box10);
-		
-		
+
 		JLabel label13 = new JLabel("피부가 가렵다");
 		JCheckBox box11 = new JCheckBox("YES");
 		box9.addItemListener(new ItemListener() {
@@ -255,18 +280,17 @@ public class Main extends JFrame {
 
 			@Override
 			public void itemStateChanged(ItemEvent e) {
-				count += 0;
+				count += 1;
 			}
 
 		});
-		
+
 		label13.setBounds(250, 280, 200, 100);
 		box11.setBounds(290, 340, 100, 50);
 		box12.setBounds(290, 380, 100, 50);
 		panel4.add(label13);
 		panel4.add(box11);
 		panel4.add(box12);
-
 
 		JButton btn10 = new JButton("취소");
 		JButton btn11 = new JButton("다음");
@@ -286,56 +310,178 @@ public class Main extends JFrame {
 		});
 
 		btn11.addActionListener(new ActionListener() {
-			
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				panel4.setVisible(false);
-				panel5.setVisible(true);
-				String coment ="";
-				if(count > 0 || count < 21) {
-					coment = "건강하시네요!!!!";
-				}else if(count < 41){
-					coment = "정상범위 이내입니다 좀더 노력하세요!";
-				}else if(count < 61) {
-					coment = "건강 좀 챙기셔야해요!!!!";
-				}else if(count < 81) {
-					coment = "당신 위험해!!!!!";
-				}else {
-					coment = "어떻게 살아있지???";
-				}
-				
-				
-				
-				counts = Integer.toString(count)+"점" + "\t" +coment;
-
-				// 점수 체크 메뉴
-				JButton btn20 = new JButton("확인");
-				btn20.setLocation(200, 400);
-				btn20.setSize(100, 50);
-				btn20.addActionListener(new ActionListener() {
-
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						panel5.setVisible(false);
-						panel1.setVisible(true);
-					}
-
-				});
-				panel5.setLayout(null);
-				panel5.setBounds(0, 0, 500, 700);
-				JLabel label15 = new JLabel("건강 점수 및 코맨트 : " + counts);
-				
-				
-				
-				label15.setLocation(100, 100);
-				label15.setSize(500, 100);
-				panel5.add(label15);
-				panel5.add(btn20);
+				panel6.setVisible(true);
 
 			}
 
 		});
+
+		panel6.setBounds(0, 0, 500, 700);
+		panel6.setLayout(null);
+
+		JLabel label30 = new JLabel("손톱이 하얗거나 세로줄이 생긴다");
+		JCheckBox box20 = new JCheckBox("YES");
+		box20.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count += 10;
+
+			}
+
+		});
+
+		JCheckBox box21 = new JCheckBox("NO");
+
+		label30.setBounds(20, 5, 200, 100);
+		box20.setBounds(60, 60, 100, 50);
+		box21.setBounds(60, 100, 100, 50);
+
+		panel6.add(label30);
+		panel6.add(box20);
+		panel6.add(box21);
+
+		JLabel label31 = new JLabel("신체에 붉은 반점이 일어난다");
+		JCheckBox box22 = new JCheckBox("YES");
+		box22.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count += 10;
+			}
+
+		});
+
+		JCheckBox box23 = new JCheckBox("NO");
+
+		label31.setBounds(250, 5, 200, 100);
+		box22.setBounds(290, 60, 100, 50);
+		box23.setBounds(290, 100, 100, 50);
+
+		panel6.add(label31);
+		panel6.add(box22);
+		panel6.add(box23);
+		
+		
+		JLabel label32 = new JLabel("다리가 저리다");
+		JCheckBox box24 = new JCheckBox("Yes");
+		box24.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count +=10;
+			}
+			
+		});
+		
+		JCheckBox box25 = new JCheckBox("NO");
+		label32.setBounds(250, 280, 200, 100);
+		box24.setBounds(290, 340, 100, 50);
+		box25.setBounds(290, 380, 100, 50);
+		
+		panel6.add(label32);
+		panel6.add(box24);
+		panel6.add(box25);
+
+		
+		
+		JLabel label33 = new JLabel("손에 땀이 찬다");
+		JCheckBox box26 = new JCheckBox("YES");
+		box26.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				count +=10;
+			}
+			
+		});
+		
+		JCheckBox box27 = new JCheckBox("NO");
+		label33.setBounds(20, 280, 200, 100);
+		box26.setBounds(60, 340, 100, 50);
+		box27.setBounds(60, 380, 100, 50);
+		
+		panel6.add(label33);
+		panel6.add(box26);
+		panel6.add(box27);
+		
+		
+		
+		
+		
+		JButton btn30 = new JButton("취소");
+		JButton btn31 = new JButton("다음");
+
+		btn30.setBounds(140, 610, 80, 50);
+		btn31.setBounds(240, 610, 80, 50);
+
+		btn30.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel4.setVisible(false);
+				panel1.setVisible(true);
+
+			}
+
+		});
+
+		btn31.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String coment = "";
+				if (count < 21) {
+					coment = "건강하시네요!!!!";
+				} else if (count < 41) {
+					coment = "정상범위 이내입니다 좀더 노력하세요!";
+				} else if (count < 61) {
+					coment = "건강 좀 챙기셔야해요!!!!";
+				} else if (count < 81) {
+					coment = "당신 위험해!!!!!";
+				} else {
+					coment = "어떻게 살아있지???";
+				}
+				// 라벨값 재설정
+				counts = Integer.toString(count) + "점" + "\t" + coment;
+				count = 0;
+				label15.setText(counts);
+				label15.repaint();
+				// 체크 박스 초기화
+				box1.setSelected(false);
+				box2.setSelected(false);
+				box3.setSelected(false);
+				box4.setSelected(false);
+				box5.setSelected(false);
+				box6.setSelected(false);
+				box7.setSelected(false);
+				box8.setSelected(false);
+				box9.setSelected(false);
+				box10.setSelected(false);
+				box11.setSelected(false);
+				box12.setSelected(false);
+				box20.setSelected(false);	
+				box21.setSelected(false);
+				box22.setSelected(false);
+				box23.setSelected(false);
+				box24.setSelected(false);
+				box25.setSelected(false);
+				box26.setSelected(false);
+				box27.setSelected(false);
+
+				panel6.setVisible(false);
+				panel5.setVisible(true);
+
+			}
+
+		});
+		panel6.add(btn30);
+		panel6.add(btn31);
+		
 
 		panel4.add(label10);
 		panel4.add(label11);
@@ -356,6 +502,7 @@ public class Main extends JFrame {
 		this.add(panel3);
 		this.add(panel4);
 		this.add(panel5);
+		this.add(panel6);
 
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
