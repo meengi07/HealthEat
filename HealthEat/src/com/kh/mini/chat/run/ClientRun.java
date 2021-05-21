0707abc03bf5eb1aaab3bdce553968daadc41321
iminjae <iminjae@localhost>
2021-05-21 12:47:32 +0900

    이민재 채팅 추가

---
+++ HealthEat/src/com/kh/mini/chat/run/ClientRun.java
@@ -0,0 +1,14 @@
+package chat.run;
+
+import chat.controller.Client;
+
+public class ClientRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new Client().connect();
+	
+	}
+
+}
