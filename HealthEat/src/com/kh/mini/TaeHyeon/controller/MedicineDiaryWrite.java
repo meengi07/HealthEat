b0e26f65f8276d2d6ddf8f0dd8b5a9f5b05976b5
kh-b1 <zmfozjko0465@gmail.com>
2021-05-23 17:26:41 +0900

    asdlfkjaklsj

--- HealthEat/src/com/kh/mini/TaeHyeon/controller/MedicineDiaryWrite.java
+++ HealthEat/src/com/kh/mini/TaeHyeon/controller/MedicineDiaryWrite.java
@@ -3,13 +3,24 @@ package com.kh.mini.TaeHyeon.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 public class MedicineDiaryWrite extends JFrame implements ActionListener{
 	
 	JLabel todayDate = new JLabel("현재 날짜", JLabel.CENTER);
+	JLabel textDate = new JLabel();
+	JTextArea textAreaWriteUp = new JTextArea();
+	JButton svaeButton = new JButton("등록하기");
+	JButton cancelButton = new JButton("취소하기");
 	
+	public MedicineDiaryWrite() {
+		
+		// 이 텍스트 저장 파일이 현재 복용 일기 작성 란과 연동되어야 함
+		
+	}
 	
 	
 	@Override
