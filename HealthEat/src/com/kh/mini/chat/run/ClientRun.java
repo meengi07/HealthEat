993fd29b5125255be5bd7a7fb2ec7eb6b77663f9
iminjae <iminjae@localhost>
2021-05-24 09:42:21 +0900

    채팅넣기

---
+++ HealthEat/src/com/kh/mini/chat/run/ClientRun.java
@@ -0,0 +1,14 @@
+package com.kh.tm1.chat.run;
+
+import com.kh.tm1.chat.controller.Client;
+
+public class ClientRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new Client().connect();
+	
+	}
+
+}
---
+++ HealthEat/src/com/kh/mini/chat/run/ServerRun.java
@@ -0,0 +1,15 @@
+package com.kh.tm1.chat.run;
+
+import com.kh.tm1.chat.controller.Server;
+
+public class ServerRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new Server().serverStart();
+		
+	}
+
+
+}
